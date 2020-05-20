7c263b886b6f3e00ac2610c600d5ce90558fafcb
StijnHeemskerk1 <stijnheemskerk@hotmail.nl>
2020-05-20 13:30:30 +0200

    6 af

--- 6-Functions/taak01/index.php
+++ 6-Functions/taak01/index.php
@@ -8,7 +8,13 @@
 <body>
 
 	<?php 
-	
+		function zetTweeWoordenAanElkaar(){
+			$woordje = "Hallo ";
+			$zinnetje = "mooie blauwe planneet!";
+			$heleZin = $woordje . $zinnetje;
+			echo $heleZin;
+		}
+		zetTweeWoordenAanElkaar();
 	?>
 
 </body>
